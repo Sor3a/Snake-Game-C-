c19a2d06cab6c5f79b59ca87d220ad65206b1249
sor3a <lounissirayene@yahoo.fr>
2023-08-30 16:16:55 +0100

    added thread for updating the game , the rendering is on the main thread

--- Source.cpp
+++ Source.cpp
@@ -3,24 +3,43 @@
 #include"Assets/Scenes/Scene/SnakeScene/SnakeScene.h"
 #include "Assets/Scenes/SceneManager.h"
 #include"Assets/Scenes/Scene/ReplayGameScene/lostScene.h"
+#include <thread>
+#include <mutex>
 
+std::mutex m;
+
+void updateScene(bool& windowOpen, Scene*& scene_)
+{
+    while (windowOpen)
+    {
+        std::lock_guard<std::mutex> guard(m);
+        scene_->Update();
+        scene_ = SceneManager::getCurrentScene(); // update if the scene has changed
+
+    };
+}
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Snake");
+    sf::RenderWindow window(sf::VideoMode(500, 500), "Snake");
 
     //Scene creations
     LostScene lostScene;
-    SnakeScene SnakeScene(80,80,500);
+    SnakeScene SnakeScene(40,40,250);
     SceneManager::addScene(&SnakeScene,0);
     SceneManager::addScene(&lostScene, 1);
     SceneManager::ChangeScene(0);
     Scene* scene = SceneManager::getCurrentScene();
     //scene->Start();
 
-
+    //auto updateScene = [&](Scene* & scene_)
+    //{
+    //    
+    //};
+    bool windowOpen = true;
+    std::thread UpdatingSceneThread(updateScene,std::ref(windowOpen),std::ref(scene));
+    
     while (window.isOpen())
     {
-
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -29,12 +48,15 @@ int main()
         }
         window.clear();
 
-        scene->Update();
+        m.lock();
         scene->Draw(window);
-        scene = SceneManager::getCurrentScene(); // update if the scene has changed
+        m.unlock();
+        
+        
 
         window.display();
     }
-
+    windowOpen = false;
+    UpdatingSceneThread.join();
     return 0;
 }
